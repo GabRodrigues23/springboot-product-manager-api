4d277263e329b07c7036a8de3355745005c6f4f0
GabRodrigues23 <gabrigues23@gmail.com>
2025-03-12 19:26:56 -0300

    NEW: Create Client Entity

---
+++ src/main/java/com/spring/manager/entity/Client.java
@@ -0,0 +1,64 @@
+package com.spring.manager.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "CLIENT")
+public class Client {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    @Column(nullable = false, length = 50)
+    private String name;
+    
+    @Column(length = 14)
+    private String cpf;
+
+    @Column(length = 10) 
+    private String cep;
+    
+    @Column(length = 15)
+    private String phone;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}

28e4f3b18ee44f1523bc47d0a6e699c08197e1ec
GabRodrigues23 <gabrigues23@gmail.com>
2025-03-12 13:08:24 -0300

    NEW: Create Product Entity

---
+++ src/main/java/com/spring/manager/entity/Product.java
@@ -0,0 +1,86 @@
+package com.spring.manager.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import org.hibernate.annotations.*;
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="PRODUCT")
+public class Product {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, unique = true, length = 13)
+    private String barcode;
+    
+    @Column(nullable = false, length = 200)
+    private String description;
+    
+    @Column(nullable = false, length = 4)
+    private Character unit;
+    
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+    
+    @Column(nullable = false)
+    private long stock;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updateAt;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Character getUnit() {
+        return unit;
+    }
+
+    public void setUnit(Character unit) {
+        this.unit = unit;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public long getStock() {
+        return stock;
+    }
+
+    public void setStock(long stock) {
+        this.stock = stock;
+    }
+}

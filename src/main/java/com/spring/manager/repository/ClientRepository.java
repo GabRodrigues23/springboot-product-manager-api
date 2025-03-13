d376e64945eef3a1140c07e577f55534aef5d645
GabRodrigues23 <gabrigues23@gmail.com>
2025-03-12 22:03:41 -0300

    NEW: Create ClientRepository

---
+++ src/main/java/com/spring/manager/repository/ClientRepository.java
@@ -0,0 +1,8 @@
+package com.spring.manager.repository;
+
+import com.spring.manager.entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long>{
+    
+}
